e68968737ace67b858d48188ba8e7a8faa8de9ef
Timofey Ivashchenko <tymofii.ivashchenko@gmail.com>
2025-11-24 12:44:47 +0200

    Добавлен логгинг

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import logging
+
 from dotenv import load_dotenv
 from os import getenv
 from telegram import (
@@ -10,12 +12,20 @@ from telegram.ext import (
     ContextTypes,
 )
 
+logging.basicConfig(
+    format="[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s",
+    level=logging.INFO,
+)
+
+logger = logging.getLogger(__name__)
+
 def main():
     load_dotenv()
     BOT_TOKEN = getenv("BOT_TOKEN")
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CallbackQueryHandler(callback_handler))
+    logger.info("Bot started...")
     app.run_polling()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
